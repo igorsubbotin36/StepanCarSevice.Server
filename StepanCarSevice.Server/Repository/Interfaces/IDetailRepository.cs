22db04fe906c1e1e0ef0091f5761cf62610ec76d
Степан Кораблев <cyb_1997@mail.ru>
2025-07-10 14:35:46 +0300

    Added Detail repository

---
+++ StepanCarSevice.Server/Repository/Interfaces/IDetailRepository.cs
@@ -0,0 +1,15 @@
+﻿using StepanCarSevice.Server.Entities;
+using StepanCarSevice.Server.Models;
+
+namespace StepanCarSevice.Server.Repository.Interfaces
+{
+    public interface IDetailRepository
+    {
+        public Task<List<Detail>> GetDetailByCode(string code);
+        public Task<List<Detail>> GetAllDetails();
+        public Task<Detail?> GetDetailById(int id);
+        public Task<bool> AddDetail(Detail detail);
+        public Task<bool> EditDetail(Detail detail);
+        public Task<bool> DeleteDetail(int id);
+    }
+}
