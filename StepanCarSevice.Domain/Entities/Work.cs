e9f5924c2fc885505232de0f9c8daa6e09e139ae
Субботин Игорь <i.subbotin@ucs-service.ru>
2026-02-02 16:32:10 +0300

    Reapply "Deleting Starting projects"
    This reverts commit e83b59501a7e7a5c7fee74a8a91990bf8e967085.

--- StepanCarSevice.Domain/Entities/Work.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StepanCarSevice.Domain.Entities
-{
-    public class Work
-    {
-        public int Id { get; set; }
-        public required string Name { get; set; }
-        public decimal Price { get; set; }
-    }
-}
