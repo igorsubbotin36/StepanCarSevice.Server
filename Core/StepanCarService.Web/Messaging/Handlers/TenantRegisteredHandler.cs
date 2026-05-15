8b90a4f8952216e0e62340a1801b42f5273d6c69
Субботин Игорь <i.subbotin@ucs-service.ru>
2026-05-15 15:59:40 +0300

    RabbitMQ Consumer Handler moved to messaging/handler

--- Core/StepanCarService.Web/Messaging/Handlers/TenantRegisteredHandler.cs
+++ Core/StepanCarService.Web/Messaging/Handlers/TenantRegisteredHandler.cs
@@ -9,7 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StepanCarService.Web.Handlers
+namespace StepanCarService.Web.Messaging.Handlers
 {
     public class TenantRegisteredHandler<T> : ITenantRegisteredHandler where T : ITenantRepository
     {
