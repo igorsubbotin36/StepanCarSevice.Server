058df953cea5339078981aeb3a67c72001c4d919
Субботин Игорь <i.subbotin@ucs-service.ru>
2026-05-26 14:29:38 +0300

    Added DTOs

---
+++ Services/Detail/StepanCarSevice.DetailService.Application/Models/DTO/DeatilServiceDtos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepanCarSevice.DetailService.Application.Models.DTO
+{
+    public record CarManudactureDto(string NameEng, string NameRu, string Country, string TenantId);
+    public record CarModelDto(int ManufactureId, string NameEng, string NameRu, int YearFrom, int YearTo, string TenantId);
+    public record DetailManufactureDto(string Name, string TenantId);
+    public record DetailDto(string Code, string OriginalCode, int DetailManufactureId, string Name, int CarModelId, decimal Price, int Count, string TenantId);
+}
