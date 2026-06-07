ebe418fbbf6afc475cfadfd3bbbb103331a36c90
Субботин Игорь <i.subbotin@ucs-service.ru>
2026-06-07 22:19:23 +0300

    Added CarModification to DTOs

--- Services/Detail/StepanCarSevice.DetailService.Application/Models/DTO/DeatilServiceCreateDtos.cs
+++ Services/Detail/StepanCarSevice.DetailService.Application/Models/DTO/DeatilServiceCreateDtos.cs
@@ -8,6 +8,7 @@ namespace StepanCarSevice.DetailService.Application.Models.DTO
 {
     public record CarManufactureCreateDto(string NameEng);
     public record CarModelCreateDto(int ManufactureId, string Name, int YearFrom, int YearTo);
+    public record CarModificationCreateDto(int CarModelId, string EngineName, int EnginePower, int WheelDriveTypeId, int TransmissionTypeId);
     public record DetailManufactureCreateDto(string Name);
     public record DetailCreateDto(string Code, string OriginalCode, int DetailManufactureId, string Name, decimal Price, int Count);
 }
--- Services/Detail/StepanCarSevice.DetailService.Application/Models/DTO/DetailServiceUpdateDto.cs
+++ Services/Detail/StepanCarSevice.DetailService.Application/Models/DTO/DetailServiceUpdateDto.cs
@@ -8,6 +8,7 @@ namespace StepanCarSevice.DetailService.Application.Models.DTO
 {
     public record CarManufactureUpdateDto(int Id, string? Name);
     public record CarModelUpdateDto(int Id, int ManufactureId, string? Name, int? YearFrom, int? YearTo);
+    public record CarModificationUpdateDto(int id, int? CarModelId, string? EngineName, int? EnginePower, int? WheelDriveTypeId, int? TransmissionTypeId);
     public record DetailManufactureUpdateDto(int Id, string? Name);
     public record DetailUpdateDto(int Id, string? Code, string? OriginalCode, int DetailManufactureId, string? Name, decimal? Price, int? Count);
 }
