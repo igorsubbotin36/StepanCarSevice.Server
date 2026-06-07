6316a8f431e2f5ed61ef7b7587820e4ebeb2ab65
Субботин Игорь <i.subbotin@ucs-service.ru>
2026-06-07 22:22:33 +0300

    Added CarModification mappers

---
+++ Services/Detail/StepanCarSevice.DetailService.Application/Interfaces/Mappers/ICarModificationMappers.cs
@@ -0,0 +1,17 @@
+﻿using StepanCarSevice.DetailService.Application.Models.DTO;
+using StepanCarSevice.DetailService.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepanCarSevice.DetailService.Application.Interfaces.Mappers
+{
+    public interface ICarModificationMappers
+    {
+        CarModificationReadDto CarModificationToReadDto(CarModification carModel);
+        CarModification UpdateDtoToCarModification(CarModificationUpdateDto model);
+        CarModification CreateDtoToCarModification(CarModificationCreateDto model);
+    }
+}
